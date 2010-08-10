b6a6341ca4f5c071adb0da7188c4b019dd880bc3
Muthu Shanmugam <muthu@muthu-shanmugams-macbook-pro.local>
2010-08-10 15:13:12 -0400

    ec2_manager part 2

--- spec/amazon_cloud/ec2_manager_spec.rb
+++ spec/amazon_cloud/ec2_manager_spec.rb
@@ -1,20 +1,64 @@
 require "spec_helper"
+require "lib/amazon_cloud/ec2_manager"
 
 describe "EC2Manager" do
   context "with no build servers running" do
-    it "should start a server"   
-    it "should return server details"
-  end
-  context "with only build server busy" do
-    context "and project have been commited long time ago (>5 minutes)" do
-    it "should start a second server"
-    it "should return server details"                                  
+    before :each do
+      @ec2Manager = EC2Manager.new        
+      @project = {:project_id => 1, :committed_on => 2.minutes.ago}
     end
-    context "and project have been committed recently (<5 minutes)" do
-      it "should NOT start a second server"
-      it "should say 'no servers available - try after 5 mins'"
+    it "should start a server" do        
+      @ec2Manager.running_count.should equal(0) 
+      lambda {
+        server = @ec2Manager.get_server(@project)      
+      }.call
+      @ec2Manager.running_count.should equal(1)      
+    end   
+    it "should return server details" do
+      server = @ec2Manager.get_server(@project)
+      server.should have_key(:instance_id)
     end
   end                                               
+  
+  context "with only build server busy" do
+    context "and project have been commited long time ago (>5 minutes)" do
+    before :each do
+      @ec2Manager = EC2Manager.new
+      @ec2Manager.get_server({:project_id => 1, :committed_on => 6.minutes.ago})
+      @project = {:project_id => 2, :committed_on => 6.minutes.ago}
+    end
+    it "should start a second server" do
+      @ec2Manager.running_count.should equal(1)
+      @ec2Manager.get_server(@project)
+      @ec2Manager.running_count.should equal(2)
+    end
+    it "should return server details" do                                  
+      server = @ec2Manager.get_server(@project)
+      server.should have_key(:instance_id)     
+    end
+    end
+   
+    context "and project have been committed recently (<5 minutes)" do
+      before :each do
+        @ec2Manager = EC2Manager.new
+        @ec2Manager.get_server({:project_id => 1, :committed_on => 6.minutes.ago})
+        @project = {:project_id => 2, :committed_on => 2.minutes.ago}
+      end
+      it "should NOT start a second server" do
+        @ec2Manager.running_count.should equal(1)
+        catch(:try_after_5_mins) do
+          @ec2Manager.get_server(@project)
+        end
+        @ec2Manager.running_count.should equal(1)
+      end
+      it "should say 'try after 5 mins'" do   
+        lambda {
+          @ec2Manager.get_server(@project)
+        }.should throw_symbol(:try_after_5_mins)
+      end
+    end
+  end
+                                                 
   context "with one server idle for more than 1 hour" do
     it "should stop the idle server"
   end
