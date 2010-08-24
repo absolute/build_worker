5f143c29a25e9aa14a64548c069d6cd24ed033df
Muthu Shanmugam <muthu@muthu-shanmugams-macbook-pro.local>
2010-08-24 14:54:10 -0400

    svn task cleaned

--- lib/tasks/svn.rake
+++ lib/tasks/svn.rake
@@ -4,29 +4,53 @@ namespace "svn" do
             
 # All directories should end with /
 desc "build RubyOnRails project"
-task "checkout" do
-    Dir.chdir(%{#{ENV["DRIVE_DIR"]}}) do 
-      sh %{svn checkout #{ENV['PROJECT_URI']} #{ENV['PROJECT_NAME']}}
-    end            
-end
-directory "#{ENV['BUILD_DIR']}"
-task "update" => ["#{ENV['BUILD_DIR']}"] do     
-    Dir.chdir(%{#{ENV['DRIVE_DIR']}#{ENV['PROJECT_NAME']}}) do 
-        sh %{svn update}
-        sh %{svn log --limit 1 -v > #{ENV['BUILD_DIR']}#{ENV['COMMIT_REPORT']}}
-    end            
+task "checkout" do         
+  Dir.chdir(%{#{ENV["PROJECT_FOLDER"]}}) do 
+    sh %{svn checkout #{ENV['PROJECT_URI']} source}
+  end            
+end     
+
+directory "#{ENV['PROJECT_FOLDER']}#{ENV['BUILD_ID']}"
+task "update" => ["#{ENV['PROJECT_FOLDER']}#{ENV['BUILD_ID']}"] do   
+    source_folder = "#{ENV['PROJECT_FOLDER']}source/"  
+    build_folder = "#{ENV['PROJECT_FOLDER']}#{ENV['BUILD_ID']}/"  
+    Dir.chdir("#{ENV['PROJECT_FOLDER']}source/") do 
+      sh %{svn update}
+      sh %{svn log --limit 1 -v > ../#{ENV['BUILD_ID']}/commit.report}
+    end                                                        
     commit_details = Hash.new()
-    out = File.new("#{ENV['BUILD_DIR']}#{ENV['COMMIT_REPORT']}").readlines(nil)[0]
+    out = File.new(build_folder+"commit.report").readlines(nil)[0]
     row = out.scan(/\n([^|]+)\|([^|]+)\|([^|]+)\|[^\n]+/).flatten
     commit_details["commit_id"] = row[0].strip
     commit_details["commit_by"] = row[1].strip
     commit_details["commit_on"] = row[2].strip
     commit_details["commit_message"] = out.scan(/\n\n(.+)\n/).flatten[0].strip
     commit_details["changed_files"] = out.scan(/\n\s+([MAD] .*)/).flatten
-    File.open( "#{ENV['BUILD_DIR']}#{ENV['COMMIT_REPORT']}", 'w' ) do |out|
+    File.open(build_folder+"commit.report", 'w' ) do |out|
         YAML.dump(commit_details, out) 
     end
-    
 end               
+
+
+
+# directory "#{ENV['BUILD_DIR']}"
+# task "update" => ["#{ENV['BUILD_DIR']}"] do     
+#     Dir.chdir(%{#{ENV['DRIVE_DIR']}#{ENV['PROJECT_NAME']}}) do 
+#         sh %{svn update}
+#         sh %{svn log --limit 1 -v > #{ENV['BUILD_DIR']}#{ENV['COMMIT_REPORT']}}
+#     end            
+#     commit_details = Hash.new()
+#     out = File.new("#{ENV['BUILD_DIR']}#{ENV['COMMIT_REPORT']}").readlines(nil)[0]
+#     row = out.scan(/\n([^|]+)\|([^|]+)\|([^|]+)\|[^\n]+/).flatten
+#     commit_details["commit_id"] = row[0].strip
+#     commit_details["commit_by"] = row[1].strip
+#     commit_details["commit_on"] = row[2].strip
+#     commit_details["commit_message"] = out.scan(/\n\n(.+)\n/).flatten[0].strip
+#     commit_details["changed_files"] = out.scan(/\n\s+([MAD] .*)/).flatten
+#     File.open( "#{ENV['BUILD_DIR']}#{ENV['COMMIT_REPORT']}", 'w' ) do |out|
+#         YAML.dump(commit_details, out) 
+#     end
+#     
+# end               
 
 end
--- spec/tasks/svn_spec.rb
+++ spec/tasks/svn_spec.rb
@@ -2,111 +2,109 @@ require "spec_helper"
 
 describe "svn", "#new project" do  
   before (:all) do
-      @drive_dir = "/Users/muthu/project/svn_unittest/"
-      @project_uri = "http://indiaserver.no-ip.info/repo/dummy/trunk"
-      @project_name = "dummy/"    
-      @build_dir = "/Users/muthu/project/builds/1/"
+    @project_folder = "drive/dummy/"
+    @build_id = "123.123"
+    @project_uri = "http://indiaserver.no-ip.info/repo/dummy/trunk"
   end
   context "checkout on an empty drive" do     
     before (:each) do    
-      FileUtils.mkdir_p @drive_dir
+      FileUtils.mkdir_p @project_folder
     end
     it "should end successfully" do  
-      system(%{rake svn:checkout DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} PROJECT_URI=#{@project_uri}})    
+      system(%{rake svn:checkout PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id} PROJECT_URI=#{@project_uri}})    
       $?.success?.should == true
     end
     it "should checkout from repository" do            
-      Dir.entries("#{@drive_dir}").select {|n| n =~ /^\w/}.should be_empty
-      system(%{rake svn:checkout DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} PROJECT_URI=#{@project_uri}})    
-      Dir.entries("#{@drive_dir}").select {|n| n =~ /^\w/}.should_not be_empty    
+      File.exists?("#{@project_folder}source").should == false    
+      system(%{rake svn:checkout PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id} PROJECT_URI=#{@project_uri}})    
+      Dir.entries("#{@project_folder}source").select {|n| n =~ /^\w/}.should_not be_empty    
     end                                                  
     after (:each) do
-      FileUtils.rm_rf @drive_dir
+      FileUtils.rm_rf @project_folder
     end
   end                                 
   context "checkout on a NON-empty drive" do             
     before (:each) do
-      FileUtils.mkdir_p "#{@drive_dir}/#{@project_name}/tmp"
+      FileUtils.mkdir_p "#{@project_folder}/source/tmp"
     end
     it "should fail" do                                                             
       pending ("svn doesn't report error if trying to checkout for second time")
-      system(%{rake svn:checkout DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} PROJECT_URI=#{@project_uri}})    
+      system(%{rake svn:checkout PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id} PROJECT_URI=#{@project_uri}})    
       $?.success?.should == false      
     end    
     it "should report 'destination already exists' error" do
       pending ("svn doesn't report error if trying to checkout for second time")
-      system(%{rake svn:checkout DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} PROJECT_URI=#{@project_uri} 2>error.log 1>out.log})    
+      system(%{rake svn:checkout PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id} PROJECT_URI=#{@project_uri}})    
       File.new("error.log").readlines(nil)[0].should match(/.*destination.*already.*exists.* /)
     end
     after (:each) do    
-      FileUtils.rm_rf @drive_dir
+      FileUtils.rm_rf @project_folder
     end
   end
 end
   
 describe "svn", "#existing project" do
   before (:all) do
-      @drive_dir = "/Users/muthu/project/svn_unittest/"
-      @project_uri = "http://indiaserver.no-ip.info/repo/dummy/trunk"
-      @project_name = "dummy/"    
-      @build_dir = "/Users/muthu/project/builds/1/"
-      @commit_report = "commit.report"
+    @project_folder = "drive/dummy/"
+    @build_id = "123.123"
+    @project_uri = "http://indiaserver.no-ip.info/repo/dummy/trunk"
   end
   context "update on an empty drive" do   
     before (:each) do
-      FileUtils.mkdir_p @drive_dir
+      FileUtils.mkdir_p @project_folder
     end
     it "should fail" do
-      system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} 2>error.log 1>out.log})
+      system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id} 2>error.log 1>out.log})
       $?.success?.should == false 
     end                           
     it "show report 'No such file or directory ' error" do
-      system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} 2>error.log 1>out.log})
+      system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id} 2>error.log 1>out.log})
       File.new("error.log").readlines(nil)[0].should match(/.*No.such.file.or.directory.*/)
     end  
     after (:each) do
-      FileUtils.rm_rf @drive_dir
+      FileUtils.rm_rf @project_folder
     end
   end  
   context "update on a NON-empty drive" do
     before (:all) do
-      FileUtils.mkdir_p @drive_dir                      
-      Dir.chdir("#{@drive_dir}") do 
-        system %{svn checkout #{@project_uri} #{@project_name}}
-      end
+      FileUtils.mkdir_p @project_folder                      
+      Dir.chdir("#{@project_folder}") do 
+        system %{svn checkout #{@project_uri} source}
+      end                                           
+      @commit_report =  @project_folder+@build_id+"/commit.report"
     end
     context "with same project in drive" do
-      it "should be successfull" do
-        system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} BUILD_DIR=#{@build_dir} COMMIT_REPORT=#{@commit_report}})  
+      it "should be successfull" do      
+        system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id}})  
         $?.success?.should == true
       end
       it "should return commit id" do
-        system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} BUILD_DIR=#{@build_dir} COMMIT_REPORT=#{@commit_report} 2>error.log 1>out.log})  
-        File.new(@build_dir+@commit_report).readlines(nil)[0].should match(/.*commit_id:\s*/)   
+        system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id}})  
+        File.new(@commit_report).readlines(nil)[0].should match(/.*commit_id:\s*/)   
         
       end
       it "should return commit by" do
-        system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} BUILD_DIR=#{@build_dir} COMMIT_REPORT=#{@commit_report} 2>error.log 1>out.log})  
-        File.new(@build_dir+@commit_report).readlines(nil)[0].should match(/.*commit_by:\s*/)   
+        system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id}})  
+        File.new(@commit_report).readlines(nil)[0].should match(/.*commit_by:\s*/)   
       end
       it "should return commit on" do
-        system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} BUILD_DIR=#{@build_dir} COMMIT_REPORT=#{@commit_report} 2>error.log 1>out.log})  
-        File.new(@build_dir+@commit_report).readlines(nil)[0].should match(/.*commit_on:\s*/)   
+        system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id}})  
+        File.new(@commit_report).readlines(nil)[0].should match(/.*commit_on:\s*/)   
       end
       it "should return commit message" do
-        system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} BUILD_DIR=#{@build_dir} COMMIT_REPORT=#{@commit_report} 2>error.log 1>out.log})  
-        File.new(@build_dir+@commit_report).readlines(nil)[0].should match(/.*commit_message:\s*/)   
+        system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id}})  
+        File.new(@commit_report).readlines(nil)[0].should match(/.*commit_message:\s*/)   
       end
       it "should return changed files" do
-        system(%{rake svn:update DRIVE_DIR=#{@drive_dir} PROJECT_NAME=#{@project_name} BUILD_DIR=#{@build_dir} COMMIT_REPORT=#{@commit_report} 2>error.log 1>out.log})  
-        File.new(@build_dir+@commit_report).readlines(nil)[0].should match(/.*changed_files:\s*/)   
+        system(%{rake svn:update PROJECT_FOLDER=#{@project_folder} BUILD_ID=#{@build_id}})  
+        File.new(@commit_report).readlines(nil)[0].should match(/.*changed_files:\s*/)   
       end
     end
     context "with an invalid repository" do
       it "should return error details"
     end                       
     after (:all) do
-      FileUtils.rm_rf @drive_dir
+      FileUtils.rm_rf @project_folder
     end
   end        
 
